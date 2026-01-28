6acf7b10693d64eddbc0b24ea6327ca4a1e31da7
Andrzej Rybczak <andrzej@rybczak.net>
2026-01-28 01:34:22 +0100

    Make bench

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,10 +1,19 @@
 {-# OPTIONS_GHC -ddump-rule-firings -ddump-simpl -dsuppress-all #-}
-module MyLib (test) where
+module Main where
 
 import Optics.Core
+import Test.Tasty.Bench
 
 test
   :: (Int -> a -> a)
   -> ((Int, a), [a], (Int, Maybe a))
   -> ((Int, a), [a], (Int, Maybe a))
 test = iover (_1 % itraversed `iadjoin` _2 % itraversed `iadjoin` _3 % itraversed % _Just)
+
+xxx :: ((Int, Int), [Int], (Int, Maybe Int))
+xxx = ((1,2), [1..1000], (3, Just 4))
+
+main :: IO ()
+main = defaultMain
+  [ bench "test" $ nf (test (+)) xxx
+  ]
